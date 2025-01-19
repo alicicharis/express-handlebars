aa2c6ddd4349476865bd5c7f4f807286b8ceda1b
Haris Alicic <haris.alicic1@gmail.com>
2025-01-19 11:04:36 +0100

    updates

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,19 @@ import { Router, Request, Response } from 'express';
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => {
-  res.render('index', { title: 'Server data' });
+  // const heroData = [
+  //   {
+  //     title: 'Random title used for testing',
+  //     description:
+  //       'This is a description of the hero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec pur',
+  //     image: 'https://placehold.co/600x400',
+  //   },
+  // ];
+
+  res.render('index', {
+    title: 'Server data',
+    // heroData,
+  });
 });
 
 export default router;
